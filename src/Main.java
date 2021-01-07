1eecd2414d4b4deaa458f7109888a40732d1fbc5
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2021-01-07 12:30:34 -0700

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+/*
+=========================================
+By: Travis Dowd
+Date: 1-7-2021
+
+This is what will be launched, since it is Main
+=========================================
+ */
+
+package src;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+
+    @Override
+    public void start( Stage primaryStage ) throws Exception {
+        BorderPane pane = new BorderPane();
+        primaryStage.setTitle( "Check Yourself" );
+        primaryStage.setScene( new Scene( pane, 800, 600 ));
+        primaryStage.show();
+    }
+
+
+    public static void main( String[] args ) {
+        launch( args );
+    }
+}
