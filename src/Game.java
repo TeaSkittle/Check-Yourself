e74c1a8e6f58240533a221f25e8e33eb63fb010b
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2021-01-07 12:31:27 -0700

    Create Game.java

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+/*
+=========================================
+By: Travis Dowd
+Date: 1-27-2021
+
+This is file is for the core logic of the game
+=========================================
+ */
+
+package src;
+
+public class Game {
+    public Game(){}     // Basic constructor
+}
